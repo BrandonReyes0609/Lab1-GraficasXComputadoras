7d9f0e5517c9925f8fb0eb8f96cce5eb9e2e6a6f
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-07-18 00:11:21 -0600

    Add Polygon 3 with red fill and white border

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     let white = Color::new(255, 255, 255);
     let yellow = Color::new(255, 255, 0);
     let blue = Color::new(0, 0, 255);
+    let red = Color::new(255, 0, 0);
 
     let polygon1 = vec![
         (165, 380), (185, 360), (180, 330), (207, 345),
@@ -24,11 +25,18 @@ fn main() {
         (321, 335), (288, 286), (339, 251), (374, 302)
     ];
 
+    let polygon3 = vec![
+        (377, 249), (411, 197), (436, 249)
+    ];
+
     framebuffer.draw_thick_polygon(&polygon1, white, 3); // Grosor de 3 píxeles
     framebuffer.fill_polygon(&polygon1, yellow);
 
     framebuffer.draw_thick_polygon(&polygon2, white, 3); // Grosor de 3 píxeles
     framebuffer.fill_polygon(&polygon2, blue);
+
+    framebuffer.draw_thick_polygon(&polygon3, white, 3); // Grosor de 3 píxeles
+    framebuffer.fill_polygon(&polygon3, red);
 
     framebuffer.save_as_bmp("output.bmp").expect("Failed to save BMP file");
 }
